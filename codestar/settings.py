e1c72092a42fc5850b786dec960b0dfd5f57b22d
seanf316 <seanfinn316@gmail.com>
2023-01-20 01:02:06 +0000

    Created Post and Comment models

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,6 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-1ipfh*ki&(7_1kwu+ex2cf$4u9-=m5#=_x2iy)1m9(5__d80xs'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
